5b3c7af692e056098603f30982dd6f609b4f30d1
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-03-23 14:57:12 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'directivesProblem';
+
+  showParagraph = false;
+  log: number[] = [];
+
+  onClick() {
+    if (this.showParagraph) {
+      this.showParagraph = false;
+    } else {
+      this.showParagraph = true;
+    }
+    this.log.push(Date.now());
+  }
 }
